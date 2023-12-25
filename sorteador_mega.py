f8fc3ec1b5c6f1ebba916b53a105339c2b9dced0
adelson70 <129080230+adelson70@users.noreply.github.com>
2023-12-25 20:40:35 -0300

    add GUI

--- sorteador_mega.py
+++ sorteador_mega.py
@@ -3,3 +3,20 @@ import tkinter as tk
 from tkinter.font import Font
 from tkinter import messagebox
 from random import sample
+
+numeros = [n for n in range(1,61)]
+
+# Função para escolher os números
+def escolher_numeros(data):
+    ...
+
+
+
+# Main da Janela
+janela_principal = tk.Tk()
+# Titulo da Janela Principal
+janela_principal.title('Sorteador da Mega da Virada')
+
+
+
+janela_principal.mainloop()
